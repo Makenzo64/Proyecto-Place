6fde29b82ec385a1c953fb25e4e3c9449600544f
Makenzo <maklenzo64@gmail.com>
2021-08-10 09:46:32 -0400

    clase2(3erParcial)

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class DescriptionPlace extends StatelessWidget {
   //variables
   String textoTitulo;
--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto/review.dart';
+import 'package:proyecto/review_list.dart';
 
 import 'description_place.dart';
 
@@ -8,23 +9,28 @@ class MyHome extends StatelessWidget {
   Widget build(BuildContext context) {
     final descriptionPlace = Container(
       margin: EdgeInsets.only(
-          top: 250,
+          top: 70,
           left: 25,
           right: 25
       ),
       child: DescriptionPlace("Uyuni", 4, "Lorem Ipsum es simplemente texto de relleno de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto de relleno estándar de la industria desde la década de 1500, cuando un impresor desconocido tomó una galera de tipos y la mezcló para hacer un libro de muestras tipográficas. Ha sobrevivido no solo a cinco siglos, sino también al salto a la composición tipográfica electrónica, permaneciendo esencialmente sin cambios. Se popularizó en la década de 1960 con el lanzamiento de hojas de Letraset que contenían pasajes de Lorem Ipsum y, más recientemente, con software de autoedición como Aldus PageMaker que incluía versiones de Lorem Ipsum."),
     );
 
-    final review = Container(
+    final reviewList = Container(
       margin: EdgeInsets.only(
-          top: 300,
+          top: 20,
           left: 25,
           right: 25
       ),
-      height: 80,
-      child: Review("assets/images/persona.jpg", "Malory Ponce", "1 reviews 3 photos", 2, "Muy buen lugar para visitar")
+      child: ReviewList()
     );
-
+     //reviewList
+    final listView = ListView(
+      children: <Widget>[
+        descriptionPlace,
+        reviewList
+      ],
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -33,7 +39,7 @@ class MyHome extends StatelessWidget {
       ),
       body: Stack(
         children: <Widget>[
-          review
+          listView
         ],
       ),
     );
